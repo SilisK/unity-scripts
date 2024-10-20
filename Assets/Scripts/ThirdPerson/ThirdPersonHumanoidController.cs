d3e2538e8ab3f2bb142c9e0b5ac7685349cb0dbd
Silis Kleemoff <kleemoffdeveloper@gmail.com>
2024-10-20 15:44:10 -0400

    third person controller blends animation and movement

--- Assets/Scripts/ThirdPerson/ThirdPersonHumanoidController.cs
+++ Assets/Scripts/ThirdPerson/ThirdPersonHumanoidController.cs
@@ -1,10 +1,44 @@
 using UnityEngine;
 
+[RequireComponent (typeof(Animator))]
 public class ThirdPersonHumanoidController : MonoBehaviour
 {
-    [SerializeField] Animator animator;
+    Animator animator;
+
+    [SerializeField] ThirdPersonPlayerController playerController;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    float mainBlend;
     private void Update()
     {
-        animator.SetFloat("Main Blend", Input.GetAxis("Vertical"));
+        if(playerController == null)
+        {
+            Debug.LogWarning("This gameObject is trying to access a ThirdPersonPlayerController. Read https://github.com/SilisK/unity-scripts for more info.");
+            return;
+        }
+
+        Vector3 input = playerController.MovementInput();
+        if (playerController.INPUT_Z_ONLY())
+        {
+            mainBlend = Mathf.Lerp(mainBlend, input.z, 0.1f);
+        }
+        else if(playerController.INPUT_Z_AND_X())
+        {
+            float xBlend = input.z > 0 ? Mathf.Abs(input.x) : -Mathf.Abs(input.x);
+            mainBlend = Mathf.Lerp(mainBlend, input.z + xBlend, 0.1f);
+        }
+        else if (playerController.INPUT_X_ONLY())
+        {
+            mainBlend = Mathf.Lerp(mainBlend, Mathf.Abs(input.x), 0.1f);
+        }
+        else
+        {
+            mainBlend = Mathf.Lerp(mainBlend, 0, 0.1f);
+        }
+        animator.SetFloat("Main Blend", mainBlend);
     }
 }
--- Assets/Scripts/ThirdPerson/ThirdPersonPlayerController.cs
+++ Assets/Scripts/ThirdPerson/ThirdPersonPlayerController.cs
@@ -19,33 +19,59 @@ public class ThirdPersonPlayerController : MonoBehaviour
     float rotatationY;
     private void Update()
     {
-        if(Input.GetAxisRaw("Vertical") > 0.1f)
+        if (INPUT_Z_ONLY())
         {
             rotatationY = Mathf.LerpAngle(rotatationY, CameraTransform.eulerAngles.y, 0.1f);
-            Rigidbody.rotation = Quaternion.Euler(0, rotatationY, 0);
-            Rigidbody.MoveRotation(Rigidbody.rotation);
-        } 
+        }
+        else if (INPUT_Z_AND_X())
+        {
+            float angle = 45f * MovementInput().x * MovementInput().z;
+            rotatationY = Mathf.LerpAngle(rotatationY, CameraTransform.eulerAngles.y + angle, 0.1f);
+        }
+        else if(INPUT_X_ONLY())
+        {
+            float angle = 75f * MovementInput().x;
+            rotatationY = Mathf.LerpAngle(rotatationY, CameraTransform.eulerAngles.y + angle, 0.1f);
+        }
+        Rigidbody.rotation = Quaternion.Euler(0, rotatationY, 0);
     }
 
     void FixedUpdate()
     {
-        Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-        Vector3 movement = transform.forward * movementInput.z;
-
-        if (movementInput.magnitude > 0)
+        if(MovementInput().magnitude > 0)
         {
-            Vector3 newPosition = Rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime;
-            Rigidbody.MovePosition(newPosition);
+            Rigidbody.MovePosition(MovementPosition());
         }
     }
 
+    /// <summary>
+    /// Returns the movement input of this gameObject on the X and Z axes.
+    /// </summary>
+    public Vector3 MovementInput() => new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+    public bool INPUT_Z_ONLY() => Mathf.Abs(MovementInput().z) > 0 && MovementInput().x == 0;
+    public bool INPUT_Z_AND_X() => Mathf.Abs(MovementInput().z) > 0 && Mathf.Abs(MovementInput().x) > 0;
+    public bool INPUT_X_ONLY() => MovementInput().z == 0 && Mathf.Abs(MovementInput().x) > 0;
+
+    Vector3 MovementPosition()
+    {
+        if (INPUT_Z_ONLY() || INPUT_Z_AND_X())
+        {
+            Vector3 direction = (transform.forward * MovementInput().z);
+            return Rigidbody.position + direction * movementSpeed * Time.fixedDeltaTime;
+        }
+        else if (INPUT_X_ONLY())
+        {
+            Vector3 direction = (transform.forward * Mathf.Abs(MovementInput().x));
+            return Rigidbody.position + direction * movementSpeed * Time.fixedDeltaTime;
+        }
+        return Rigidbody.position;
+    }
 
     void InitializeRigidbody()
     {
         Rigidbody = GetComponent<Rigidbody>();
 
         Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-
     }
 }
