886943bf0d934682c9d5b7d8ea3d85041b47ca1b
Silis Kleemoff <kleemoffdeveloper@gmail.com>
2024-11-03 20:47:25 -0500

    game manager controls
    tab toggles settings panel and unlocks cursor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private void Start()
+    public Animator fadeInAnimator;
+
+    private IEnumerator Start()
     {
         Application.targetFrameRate = 60;
+
+        yield return new WaitForSeconds(1.3f);
+
+        fadeInAnimator.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!settingsPanel.activeInHierarchy) OpenSettingsPanel();
+            else CloseSettingsPanel();
+        }
+    }
+
+    [SerializeField]
+    GameObject settingsPanel;
+
+    public void OpenSettingsPanel()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        settingsPanel.SetActive(false);
     }
 }
